7b871bd7b15e6d7e088eb926bcd914f7aeb8129f
dikkedimi <vragen@mac-gek.nl>
2022-03-20 21:53:40 +0100

    cpmMinuteAverage faulty?
    Seems _cpmMinuteAverage is not returning an average, but an increasing number of counts? Line 123 of main.cpp
    ```
    counts[0]4
    logs[0]4
    counts[1]227
    counts[0]0   counts[1]227
    _cpmTotal: 18.28
    _dose: 0.12
    _cpmMinuteAverage: 0.00
     _i: 59
    _cpmMinuteAverage: 1.00
     _i: 58
    _cpmMinuteAverage: 1.00
     _i: 57
    _cpmMinuteAverage: 2.00
     _i: 56
    _cpmMinuteAverage: 4.00
     _i: 55
    _cpmMinuteAverage: 4.00
     _i: 54
    _cpmMinuteAverage: 4.00
     _i: 53
    _cpmMinuteAverage: 4.00
     _i: 52
    _cpmMinuteAverage: 6.00
     _i: 51
    _cpmMinuteAverage: 7.00
     _i: 50
    _cpmMinuteAverage: 7.00
     _i: 49
    _cpmMinuteAverage: 11.00
     _i: 48
    _cpmMinuteAverage: 12.00
     _i: 47
    _cpmMinuteAverage: 15.00
     _i: 46
    _cpmMinuteAverage: 15.00
     _i: 45
    _cpmMinuteAverage: 15.00
     _i: 44
    _cpmMinuteAverage: 17.00
     _i: 43
    _cpmMinuteAverage: 20.00
     _i: 42
    _cpmMinuteAverage: 20.00
     _i: 41
    _cpmMinuteAverage: 26.00
     _i: 40
    _cpmMinuteAverage: 27.00
     _i: 39
    _cpmMinuteAverage: 29.00
     _i: 38
    _cpmMinuteAverage: 30.00
     _i: 37
    _cpmMinuteAverage: 33.00
     _i: 36
    _cpmMinuteAverage: 34.00
     _i: 35
    _cpmMinuteAverage: 36.00
     _i: 34
    _cpmMinuteAverage: 37.00
     _i: 33
    _cpmMinuteAverage: 38.00
     _i: 32
    _cpmMinuteAverage: 38.00
     _i: 31
    _cpmMinuteAverage: 38.00
     _i: 30
    _cpmMinuteAverage: 41.00
     _i: 29
    _cpmMinuteAverage: 42.00
     _i: 28
    _cpmMinuteAverage: 44.00
     _i: 27
    _cpmMinuteAverage: 45.00
     _i: 26
    _cpmMinuteAverage: 47.00
     _i: 25
    _cpmMinuteAverage: 47.00
     _i: 24
    _cpmMinuteAverage: 49.00
     _i: 23
    _cpmMinuteAverage: 50.00
     _i: 22
    _cpmMinuteAverage: 50.00
     _i: 21
    _cpmMinuteAverage: 50.00
     _i: 20
    _cpmMinuteAverage: 52.00
     _i: 19
    _cpmMinuteAverage: 53.00
     _i: 18
    _cpmMinuteAverage: 53.00
     _i: 17
    _cpmMinuteAverage: 57.00
     _i: 16
    _cpmMinuteAverage: 60.00
     _i: 15
    _cpmMinuteAverage: 63.00
     _i: 14
    _cpmMinuteAverage: 64.00
     _i: 13
    _cpmMinuteAverage: 65.00
     _i: 12
    _cpmMinuteAverage: 65.00
     _i: 11
    _cpmMinuteAverage: 67.00
     _i: 10
    _cpmMinuteAverage: 67.00
     _i: 9
    _cpmMinuteAverage: 73.00
     _i: 8
    _cpmMinuteAverage: 75.00
     _i: 7
    _cpmMinuteAverage: 75.00
     _i: 6
    _cpmMinuteAverage: 77.00
     _i: 5
    _cpmMinuteAverage: 78.00
     _i: 4
    _cpmMinuteAverage: 78.00
     _i: 3
    _cpmMinuteAverage: 80.00
     _i: 2
    _cpmMinuteAverage: 84.00
     _i: 1
    counts[0]1
    logs[0]1
    counts[1]228
    counts[0]0   counts[1]228
    _cpmTotal: 18.24
    _dose: 0.12
    ```

---
+++ src/example-config.h
@@ -0,0 +1,64 @@
+#define TIMEZONE 2
+
+// Needed for Nodemcu v2.0
+// #define SDA 4
+// #define SCL 5
+#define SCREEN_ADDRESS 0x3C //
+#define OLED_RESET -1
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+#define Interval 1500
+#define tubeIndex 151
+
+#define PulsePin 13 //for esp12 use pin 13 (D7)
+#define LedPin 2 // doesn't do anything, should it be pin 0?
+
+#define LOG_PERIOD 5 // in seconds
+#define MAX_PERIOD 300 // in seconds
+#define ENTRIES (MAX_PERIOD / LOG_PERIOD)
+
+#define RadmonUpdateInterval 10
+#define DisplayUpdateInterval 1
+
+// hardware & services config
+#define SEND_TO_RADMON      false        // CPM will be sent to Radmon.org
+#define PRINT_TO_SERIAL     true
+#define ENABLE_WIFI         false
+
+//Debug switches
+#define debug_Display            false
+#define debug_Radmon             false
+#define debug_CPMcalc            true
+#define debug_Startup            true
+
+const char* ssid     = "SSID";
+const char* WiFipass = "PSK";
+
+const char *time_server = "nl.pool.ntp.org";
+
+const char *RadmonUser = "USER";
+const char *RadmonPass = "PASS";
+const char* RadmonHost = "radmon.org"; // No need to change this
+
+struct strConfig {
+  bool useRM = SEND_TO_RADMON;
+  bool useWiFi = ENABLE_WIFI;
+  // bool useBL = SEND_TO_BLYNK;
+  // bool useHT = SEND_TO_HTTP;
+  bool cpmSerialOn = PRINT_TO_SERIAL;
+  // bool useBME280 = USE_BME280;
+  // bool adr77 = USE_ADR77;
+  // bool useBAR = USE_BAR;
+  // bool useCelsius = CELSIUS;
+  bool debugStartup = debug_Startup;
+  bool debugDisplay = debug_Display;
+  bool debugRadmon = debug_Radmon;
+  // bool debugBlynk = debug_Blynk;
+  // bool debugHTTP = debug_HTTP;
+  // bool debugBME280 = debug_BME280;
+  bool debugCPMcalc = debug_CPMcalc;
+  // bool debugButtons = debug_Buttons;
+  // long elevation = ELEVATION;
+  ;
+}   config;
